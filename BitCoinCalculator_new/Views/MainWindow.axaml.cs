f7b63d8879a92ef6610f0284b384072adb3bcfb1
Katri Lotamõis <katrilotamois@outlook.com>
2025-12-19 15:52:51 +0200

    add background image to app

--- BitCoinCalculator_new/Views/MainWindow.axaml.cs
+++ BitCoinCalculator_new/Views/MainWindow.axaml.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Platform;
+using Avalonia.Media.Imaging;
 
 namespace BitCoinCalculator_new.Views;
 
@@ -9,9 +12,34 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        this.Title = "Bitcoin Calculator";
+
+        var canvas = new Canvas();
+
+        var screen = Screens.Primary;
+
+        var windowX = (screen.WorkingArea.Width) / 3;
+        var windowY = (screen.WorkingArea.Height) / 4;
+
+        this.Position = new PixelPoint(windowX, windowY);
+
+        // var backgroundImagePath = "avares://BitCoinCalculator_new/Assets/silly.jpg";
+        //
+        //
+        // Image background = new Image()
+        // {
+        //     Source = new Bitmap(backgroundImagePath),
+        //     Opacity = 10
+        // };
+        //
+        // Canvas.SetLeft(background, 0);
+        // Canvas.SetTop(background, 0);
+        //
+        // canvas.Children.Add(background);
+        //
+        // this.Content = canvas;
     }
-    // private void OnRegisterClicked(object? sender, PointerPressedEventArgs e)
-    // {
-    //     Console.WriteLine("Register clicked");
-    // }
+    
+    
 }
